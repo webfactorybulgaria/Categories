0ca6928e5decc2c9358c84e04d5cb7f76effb817
Nasko <nasko@webfactory.bg>
2016-07-25 12:36:29 +0300

    pagination

--- src/resources/views/admin/index.blade.php
+++ src/resources/views/admin/index.blade.php
@@ -9,7 +9,7 @@
     @include('core::admin._button-create', ['module' => 'categories'])
 
     <h1>
-        <span>@{{ models.length }} @choice('categories::global.categories', 2)</span>
+        <span>@{{ totalModels }} @choice('categories::global.categories', 2)</span>
     </h1>
 
     <div class="btn-toolbar">
@@ -18,15 +18,32 @@
 
     <div class="table-responsive">
 
-        <table st-persist="categoriesTable" st-table="displayedModels" st-safe-src="models" st-order st-filter class="table table-condensed table-main">
+        <table st-persist="categoriesTable" st-table="displayedModels" st-order st-sort-default="position" st-pipe="callServer" st-filter class="table table-condensed table-main">
             <thead>
+                 <tr>
+                    <td colspan="6" st-items-by-page="itemsByPage" st-pagination="" st-template="/views/partials/pagination.custom.html"></td>
+                </tr>
                 <tr>
                     <th class="delete"></th>
                     <th class="edit"></th>
                     <th st-sort="status" class="status st-sort">Status</th>
                     <th st-sort="image" class="image st-sort">Image</th>
-                    <th st-sort="position" st-sort-default="true" class="position st-sort">Position</th>
+                    <th st-sort="position" class="position st-sort">Position</th>
                     <th st-sort="title" class="title st-sort">Title</th>
+                </tr>
+                <tr>
+                    <td colspan="2"></td>
+                    <td>
+                        <select class="form-control" st-input-event="change keydown" st-search="status.boolean">
+                            <option value=""></option>
+                            <option value="true">Active</option>
+                            <option value="false">Not Active</option>
+                        </select>
+                    </td>
+                    <td colspan="2"></td>
+                    <td>
+                        <input st-search="title" class="form-control input-sm" placeholder="@lang('global.Search')…" type="text">
+                    </td>
                 </tr>
             </thead>
 
@@ -44,6 +61,12 @@
                         <input class="form-control input-sm" min="0" type="number" name="position" ng-model="model.position" ng-change="update(model)">
                     </td>
                     <td>@{{ model.title }}</td>
+                </tr>
+                <tr>
+                    <td colspan="5" st-items-by-page="itemsByPage" st-pagination="" st-template="/views/partials/pagination.custom.html"></td>
+                    <td>
+                        <div ng-include="'/views/partials/pagination.itemsPerPage.html'"></div>
+                    </td>
                 </tr>
             </tbody>
         </table>
